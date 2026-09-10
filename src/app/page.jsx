ac496ec3f88109d452d4d9b45fe682f1d136dac1
NicolasC-137 <snicolascosta@gmail.com>
2026-09-10 09:57:20 -0300

    novo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,16 @@ export default function Home() {
   return (
     <div >
     <h1>projeto de teste</h1>
+<p>Lorem ipsum dolor sit ame
+  t consectetur adipisicing elit. Architecto cupiditate quasi atque est iure tempora ipsa aliquam eaque incidunt
+   culpa. Vero eu
+   m rerum porro ullam voluptate optio hic facere necessitatibus.</p>
+<p>Lorem, ipsum dolor sit
+  amet consectetur adipisicing elit. Ea consectetur ullam dolorem aliquid aperiam alias et similique facilis illo 
+  dignissimos unde adipisci veniam corporis at, illum quo. Odit, ratione rem?
+</p>
+
+
     </div>
   );
 }
